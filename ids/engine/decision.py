95908e221faff25f9833521b8234e1418bdee80e
hyhilman <hilmannihri@outlook.co.id>
2018-07-11 05:15:19 +0700

    new decision tree

--- ids/engine/decision.py
+++ ids/engine/decision.py
@@ -5,125 +5,93 @@ logger = logging.getLogger('app.'+__name__)
 """
 === Run information ===
 
-Scheme:weka.classifiers.trees.J48 -C 0.25 -M 2
-Relation:     outReduced-weka.filters.unsupervised.attribute.Remove-R14,16-18,22-23,28,30-32,39-41,43-44,46-48,66,69-72-weka.filters.unsupervised.attribute.Remove-R20-weka.filters.unsupervised.attribute.Remove-R27-weka.filters.unsupervised.attribute.Remove-R48-50-weka.filters.unsupervised.attribute.Remove-R8,12
-Instances:    185679
-Attributes:   50
+Scheme:       weka.classifiers.trees.J48 -C 0.25 -M 2
+Relation:     out-weka.filters.unsupervised.attribute.RemoveUseless-M23.0-weka.filters.unsupervised.attribute.Remove-V-R53,171,76,75,80,167,145,170,172,174,154,147,132,152,169,79,155-156,135,144,151,146,82,81,91-92,74,70,78,73,149,153,150,175,179,142,139,72,71,177,93,140,176,178,96,94-95,90,84,86,85,87,21,20,22,12,14,13,15,89,10,148,11,83,88,19,168,141,180,114,166,110,115,111,113,112,68,1,52,116,121,181-weka.filters.unsupervised.attribute.Remove-V-R1-5,7,6,8-13,15,14,16-26,82-weka.filters.unsupervised.attribute.Remove-R3,7,12,15,21,24
+Instances:    117621
+Attributes:   21
               eth.eth.type
-              ip.ip.proto
-              ip.ip.flags
-              ip.ip.flags_tree.ip.flags.df
-              tcp.tcp.window_size_value
-              ip.ip.len
-              tcp.tcp.hdr_len
-              tcp.tcp.seq
-              tcp.tcp.window_size
               tcp.tcp.stream
-              tcp.tcp.flags
-              ip.ip.dst
-              tcp.tcp.port
+              ip.ip.flags
+              ip.ip.proto
               tcp.tcp.dstport
-              ip.ip.src
               tcp.tcp.srcport
-              ipv6.ipv6.nxt
-              tcp.tcp.options_tree.tcp.options.mss_tree.tcp.options.mss_val
+              tcp.tcp.window_size
+              tcp.tcp.window_size_value
+              tcp.tcp.hdr_len
               tcp.tcp.ack
-              ipv6.ipv6.plen
+              tcp.tcp.seq
+              ip.ip.len
               tcp.tcp.len
-              ip.ip.dsfield
-              ip.ip.dsfield_tree.ip.dsfield.dscp
-              tcp.tcp.flags_tree.tcp.flags.push
-              ipv6.ipv6.dst
-              tcp.tcp.flags_tree.tcp.flags.reset
-              udp.udp.length
-              udp.udp.stream
-              udp.udp.srcport
-              tcp.tcp.flags_tree.tcp.flags.ack
-              udp.udp.dstport
-              udp.udp.port
-              dns.dns.count.queries
-              dns.dns.flags_tree.dns.flags.opcode
-              dns.dns.count.add_rr
-              dns.dns.count.answers
-              dns.dns.count.auth_rr
-              dns.dns.flags_tree.dns.flags.truncated
-              ipv6.ipv6.hlim
-              dns.dns.flags_tree.dns.flags.response
-              dns.dns.flags
-              tcp.tcp.options
-              ipv6.ipv6.flow
-              tcp.tcp.flags_tree.tcp.flags.fin
-              ip.ip.ttl
-              eapol.wlan_rsna_eapol.keydes.nonce
-              mqtt.mqtt.msg
-              eapol.wlan_rsna_eapol.keydes.mic
-              ip.ip.checksum.status
+              tcp.tcp.nxtseq
+              tcp.tcp.analysis.tcp.analysis.bytes_in_flight
+              tcp.tcp.analysis.tcp.analysis.push_bytes_sent
+              tcp.tcp.flags
+              ip.ip.src_host
+              ipv6.ipv6.nxt
+              ipv6.ipv6.plen
               class
-Test mode:evaluate on training data
+Test mode:    evaluate on training data
 
 === Classifier model (full training set) ===
 
 J48 pruned tree
 ------------------
 
-eth.eth.type = 0x00000800
-|   ip.ip.proto <= 6
-|   |   ip.ip.len <= 44
-|   |   |   tcp.tcp.seq <= 0: flood_tcp4 (6766.0)
-|   |   |   tcp.tcp.seq > 0: normal (28396.0)
-|   |   ip.ip.len > 44: normal (121561.0)
-|   ip.ip.proto > 6
-|   |   ip.ip.flags = 0x00000000: flood_udp4 (10000.0)
-|   |   ip.ip.flags = 0x00000002: normal (160.0)
 eth.eth.type = 0x000086dd
-|   ipv6.ipv6.plen <= 325
-|   |   ipv6.ipv6.plen <= 24
-|   |   |   ipv6.ipv6.nxt <= 17: flood_tcp6 (8120.0)
-|   |   |   ipv6.ipv6.nxt > 17: normal (35.0)
-|   |   ipv6.ipv6.plen > 24: normal (547.0)
-|   ipv6.ipv6.plen > 325: flood_udp6 (10000.0)
-eth.eth.type = 0x00000806: normal (88.0)
+|   ipv6.ipv6.plen <= 1032
+|   |   ipv6.ipv6.plen <= 325: normal (197.0)
+|   |   ipv6.ipv6.plen > 325: udp6_flood (10000.0)
+|   ipv6.ipv6.plen > 1032: tcp6_flood (10000.0)
+eth.eth.type = 0x00000800
+|   ip.ip.flags = 0x00000002: normal (77307.0)
+|   ip.ip.flags = 0x00000000
+|   |   ip.ip.len <= 1052
+|   |   |   ip.ip.len <= 689: normal (67.0)
+|   |   |   ip.ip.len > 689: udp4_flood (10000.0)
+|   |   ip.ip.len > 1052: tcp4_flood (10000.0)
+eth.eth.type = 0x00000806: normal (44.0)
 eth.eth.type = 0x0000888e: normal (6.0)
 
-Number of Leaves  : 11
+Number of Leaves  : 	9
 
-Size of the tree : 19
+Size of the tree : 	15
 
 
-Time taken to build model: 17.9 seconds
+Time taken to build model: 2.43 seconds
 
 === Evaluation on training set ===
+
+Time taken to test model on training data: 0.29 seconds
+
 === Summary ===
 
-Correctly Classified Instances      185679              100      %
+Correctly Classified Instances      117621              100      %
 Incorrectly Classified Instances         0                0      %
 Kappa statistic                          1
 Mean absolute error                      0
 Root mean squared error                  0
 Relative absolute error                  0      %
 Root relative squared error              0      %
-Total Number of Instances           185679
+Total Number of Instances           117621
 
 === Detailed Accuracy By Class ===
 
-               TP Rate   FP Rate   Precision   Recall  F-Measure   ROC Area  Class
-                 1         0          1         1         1          1        flood_tcp4
-                 1         0          1         1         1          1        flood_tcp6
-                 1         0          1         1         1          1        flood_udp4
-                 1         0          1         1         1          1        flood_udp6
-                 1         0          1         1         1          1        normal
-Weighted Avg.    1         0          1         1         1          1
+                 TP Rate  FP Rate  Precision  Recall   F-Measure  MCC      ROC Area  PRC Area  Class
+                 1.000    0.000    1.000      1.000    1.000      1.000    1.000     1.000     normal
+                 1.000    0.000    1.000      1.000    1.000      1.000    1.000     1.000     tcp4_flood
+                 1.000    0.000    1.000      1.000    1.000      1.000    1.000     1.000     tcp6_flood
+                 1.000    0.000    1.000      1.000    1.000      1.000    1.000     1.000     udp4_flood
+                 1.000    0.000    1.000      1.000    1.000      1.000    1.000     1.000     udp6_flood
+Weighted Avg.    1.000    0.000    1.000      1.000    1.000      1.000    1.000     1.000
 
 === Confusion Matrix ===
 
-      a      b      c      d      e   <-- classified as
-   6766      0      0      0      0 |      a = flood_tcp4
-      0   8120      0      0      0 |      b = flood_tcp6
-      0      0  10000      0      0 |      c = flood_udp4
-      0      0      0  10000      0 |      d = flood_udp6
-      0      0      0      0 150793 |      e = normal
-
-
+     a     b     c     d     e   <-- classified as
+ 77621     0     0     0     0 |     a = normal
+     0 10000     0     0     0 |     b = tcp4_flood
+     0     0 10000     0     0 |     c = tcp6_flood
+     0     0     0 10000     0 |     d = udp4_flood
+     0     0     0     0 10000 |     e = udp6_flood
 """
 
 class PacketType(object):
@@ -157,35 +125,29 @@ def detect(pkt, blocker=None, unblocker=None, output=None):
     global PACKET_LIST
     detected = PACKET_LIST['MALICIOUS']['UNDETECTED']
 
-    if getattr(pkt[Ether], 'type') == int(b'0x00000800',16):
-        if getattr(pkt[IP], 'proto') <= 6:
-            if getattr(pkt[IP], 'len') <= 44:
-                if getattr(pkt[TCP], 'seq') <= 0:
-                    detected = +PACKET_LIST['MALICIOUS']['TCP4']
-                    logger.warning(detected)
-                elif getattr(pkt[TCP], 'seq') > 0:
-                    detected = +PACKET_LIST['NORMAL']
-            elif getattr(pkt[IP], 'len') > 44:
+    if getattr(pkt[Ether], 'type') == int(b'0x000086dd',16):
+        if getattr(pkt[IPv6], 'plen') <= 1032:
+            if getattr(pkt[IPv6], 'plen') <= 325:
                 detected = +PACKET_LIST['NORMAL']
-        elif getattr(pkt[IP], 'proto') > 6:
-            if getattr(pkt[IP], 'flags') == int('0x00000000',16):
-                detected = +PACKET_LIST['MALICIOUS']['UDP4']
+            elif getattr(pkt[IPv6], 'plen') > 325:
+                detected = +PACKET_LIST['MALICIOUS']['UDP6']
                 logger.warning(detected)
-            elif getattr(pkt[IP], 'flags') == int('0x00000002',16):
-                detected = +PACKET_LIST['NORMAL']
-    elif getattr(pkt[Ether], 'type') == int(b'0x000086dd',16):
-        if getattr(pkt[IPv6], 'plen') <= 325:
-            if getattr(pkt[IPv6], 'plen') <= 24:
-                if getattr(pkt[IPv6], 'nh') <= 17:
-                    detected = +PACKET_LIST['MALICIOUS']['TCP6']
-                    logger.warning(detected)
-                elif getattr(pkt[IPv6], 'nh') > 17:
-                    detected = +PACKET_LIST['NORMAL']
-            elif getattr(pkt[IPv6], 'plen') > 24:
-                detected = +PACKET_LIST['NORMAL']
-        elif getattr(pkt[IPv6], 'plen') > 325:
-            detected = +PACKET_LIST['MALICIOUS']['UDP6']
+        elif getattr(pkt[IPv6], 'plen') > 1032:
+            detected = +PACKET_LIST['MALICIOUS']['TCP6']
             logger.warning(detected)
+    elif getattr(pkt[Ether], 'type') == int(b'0x00000800',16):
+        if getattr(pkt[IP], 'flags') == int('0x00000002',16):
+            detected = +PACKET_LIST['NORMAL']
+        elif getattr(pkt[IP], 'flags') == int('0x00000000',16):
+            if getattr(pkt[IP], 'len') <= 1052:
+                if getattr(pkt[IP], 'len') <= 1052:
+                    detected = +PACKET_LIST['NORMAL']
+                elif getattr(pkt[IP], 'len') <= 1052:
+                    detected = +PACKET_LIST['MALICIOUS']['UDP4']
+                    logger.warning(detected)
+            elif getattr(pkt[IP], 'len') > 1052:
+                detected = +PACKET_LIST['MALICIOUS']['TCP4']
+                logger.warning(detected)
     elif getattr(pkt[Ether], 'type') == int(b'0x00000806',16):
         detected = +PACKET_LIST['NORMAL']
     elif getattr(pkt[Ether], 'type') == int(b'0x0000888e',16):
